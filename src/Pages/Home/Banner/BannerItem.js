eb1049709ba6fccc72dcca8f52dcb40a1e9b1b08
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-05-14 20:04:38 +0600

    banner creation done

--- src/Pages/Home/Banner/BannerItem.js
+++ src/Pages/Home/Banner/BannerItem.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './BannerItem.css';
 
-const BannerItem = ({ image }) => {
+const BannerItem = ({ slide }) => {
+
+    const { image, id, prev, next } = slide;
+
     return (
-        <div id="slide1" className="carousel-item relative w-full">
+        <div id={`slide${id}`} className="carousel-item relative w-full">
             <div className='carousel-img'>
-                <img src={image} alt='' className="w-full" />
+                <img src={image} alt='' className="w-full rounded-xl" />
             </div>
             <div className="absolute flex justify-end transform -translate-y-1/2 left-24 top-1/4">
                 <h1 className='text-6xl font-bold text-white'>
@@ -23,8 +27,8 @@ const BannerItem = ({ image }) => {
                 <button className="btn btn-outline btn-warning">Warning</button>
             </div>
             <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
-                <a href="#slide4" className="btn btn-circle mr-5">❮</a>
-                <a href="#slide2" className="btn btn-circle">❯</a>
+                <a href={`#slide${prev}`} className="btn btn-circle mr-5">❮</a>
+                <a href={`#slide${next}`} className="btn btn-circle">❯</a>
             </div>
         </div>
     );
